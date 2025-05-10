46b6087d0ac48272ced8efb18189a5b10bb30359
egd060708 <2231625449@qq.com>
2025-05-11 00:44:15 +0800

    补充注释，完善接口默认参数

--- controllers/serialWb_controller/serialWb_controller.cpp
+++ controllers/serialWb_controller/serialWb_controller.cpp
@@ -59,10 +59,10 @@ int main(int argc, char** argv) {
     ManipulatorS_Classdef left_manipulator(infantry_state.dt);
     Manipulator_Controller_Classdef controller(&infantry_state, &right_manipulator, &left_manipulator, &user_params, ctrlMode::MPC_);
     //qpoasesInterface mpcCal(10,4,4,0,25, PL_NONE);// qpOASES
-    tinympcInterface mpcCal(10, 4, 4, 0, 3, 1., 1, 0);// tinyMPC
+    //tinympcInterface mpcCal(10, 4, 4, 0, 3, 1., 1, 0);// tinyMPC
     //quadprogInterface mpcCal(10, 4, 4, 0, 25);//qp++
     //osqpInterface mpcCal(10, 4, 4, 0, 5);//osqp
-    //osqpeInterface mpcCal(10, 4, 4, 0, 1, false);//osqp-eigen
+    osqpeInterface mpcCal(10, 4, 4, 0, 1, false);//osqp-eigen
     //OsqpEigen::Solver solver;
     modelFit<10, 10, 3> modelA;
     modelFit<10, 4, 3> modelB;
